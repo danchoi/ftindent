968339b63eee1a6aeef73c94b062db8943aaf583
Daniel Choi <dhchoi@gmail.com>
2016-02-19 15:54:53 -0500

    sketching parsing

--- Main.hs
+++ Main.hs
@@ -9,6 +9,7 @@ import Text.Parsec
 import qualified Text.Parsec.Token as T (GenTokenParser(..))
 import Text.Parsec.Language
 import qualified Data.Text.IO as T
+import Data.Monoid
 
 
 -- parse :: Stream s Identity t => Parsec s () a -> SourceName -> s -> Either ParseError a
@@ -20,16 +21,54 @@ import qualified Data.Text.IO as T
 --  numbers = commaSep integer
 
 
+-- type signature parsing
+{-
+type Identifier = String
 
+data DoubleColon = DoubleColon Int   -- ^ position
+
+data TypeParam = TypeParam Identifier   (Maybe Comment)
+               | FuncTypeParam [TypeParam]
+
+data Function = Function Identifier DoubleColon [TypeParam]
+-}
+
+T.TokenParser{..} = haskell
+
+data FunctionType = FunctionType String Int [TypeParam]
+    deriving (Show)
+
+data TypeParam = TypeParam String 
+          | FuncTypeParam [TypeParam]
+        deriving (Show)
+
+parser :: Parsec String () FunctionType
+parser = do 
+    whiteSpace 
+    f <- identifier
+    c <- symbol "::" 
+    n <- getPosition
+    xs <- typeParams
+    return $ FunctionType f (sourceColumn n) xs
+
+typeParams :: Parsec String () [TypeParam]
+typeParams = sepBy (typeParam <|> functionParam) (symbol "->")
+
+typeParam :: Parsec String () TypeParam
+typeParam = TypeParam <$> identifier
+
+functionParam :: Parsec String () TypeParam
+functionParam = do
+    xs <- between (symbol "(") (symbol ")") typeParams
+    return $ FuncTypeParam xs
 
 main :: IO ()
 main = do
     s <- getContents
-    case (parse numbers "" s) of
+    case (parse parser "" s) of
             Left err  -> print err
-            Right (xs ) -> print . length $ xs
+            Right (xs ) -> print xs
 
-T.TokenParser{..} = haskell
 
 numbers :: Parsec String () [Integer]
 -- numbers = (commaSep haskell) (integer haskell)
